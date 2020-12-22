086385ddb9ffe1b6d5a546f2e38730db002fe0ff
Glitch (hello-express) <none>
2020-12-22 17:39:33 +0000

    💗📫 Checkpoint ./server.js:5277247/4993 ./package.json:5277247/1248

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-// http is a standard module that comes with Node.js
-const http = require('http')
+// values for the enviroment variables set in the .env file can be accesed at proces.env.VARIABLE_NAME
+const secret = process.env.WEBHOOK_SECRET
 
+const http = require('http')
+const webHookHandler = require('github-webhook-handler')({
+  path: '/',
+  secret: secret
+})
 http.createServer(handleRequest).listen(process.env.PORT)
 
+webHookHandler.on('issues', (event) => {
+  console.log(event.payload)
+  // console.log(`Received issue event for "${event.payload.issue.title}"`)
+})
+
 function handleRequest (request, response) {
-
-  // log request method & URL
-  console.log(`${request.method} ${request.url}`)
-
-  // for GET (and other non-POST) requests show "ok" and stop here
+  // ignore all requests that aren’t POST requests
   if (request.method !== 'POST') return response.end('ok')
 
-  // for POST requests, read out the request body, log it, then show "ok" as response
-  let payload = ''
-  request.on('data', (data) => payload += data );
-  request.on('end', () => {
-    console.log(payload)
-    response.end('ok')
-  })
+  // here we pass the current request & response to the webHookHandler we created
+  // on top. If the request is valid, then the "issue" above handler is called
+  webHookHandler(request, response, () => response.end('ok'))
 }
